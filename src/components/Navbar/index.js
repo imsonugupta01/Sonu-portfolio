ab7021283f0ebc7f5fde3cedd19f144d2bf086bf
SaloniSonu <159940686+SaloniSonu@users.noreply.github.com>
2024-05-11 05:34:35 +0530

    ii

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,9 +1,8 @@
 import React from 'react'
-import { Nav, NavLink, NavbarContainer, Span, NavLogo, NavItems, GitHubButton, ButtonContainer, MobileIcon, MobileMenu, MobileNavLogo, MobileLink } from './NavbarStyledComponent'
+import { Nav, NavLink, NavbarContainer, Span, NavLogo, NavItems, GitHubButton, ButtonContainer, MobileIcon, MobileMenu, MobileLink } from './NavbarStyledComponent'
 import { DiCssdeck } from 'react-icons/di';
 import { FaBars } from 'react-icons/fa';
 import { Bio } from '../../data/constants';
-import { Close, CloseRounded } from '@mui/icons-material';
 import { useTheme } from 'styled-components';
 
 const Navbar = () => {
@@ -30,7 +29,9 @@ const Navbar = () => {
           <NavLink href='#education'>Education</NavLink>
         </NavItems>
         <ButtonContainer>
-          <GitHubButton href={Bio.github} target="_blank">Github Profile</GitHubButton>
+          
+          {Bio.github && <GitHubButton href={Bio.github} target="_blank">Github Profile</GitHubButton>}
+
         </ButtonContainer>
         {
           isOpen &&
